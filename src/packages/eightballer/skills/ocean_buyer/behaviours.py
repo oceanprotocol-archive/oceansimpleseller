d0b75efe8b545392379436e4493a87a7a66b785b
David Hunt-Mateo <david@oceanprotocol.com>
2022-05-02 13:29:45 -0400

    Populate all 5 arguments when creating DOWNLOAD_JOB message
      * Fix "error: 'datatoken_address' content is not set."

--- src/packages/eightballer/skills/ocean_buyer/behaviours.py
+++ src/packages/eightballer/skills/ocean_buyer/behaviours.py
@@ -188,13 +188,12 @@ class OceanC2DBehaviour(Behaviour):
         if strategy.purchased_data is not None and not strategy.has_purchased_datatoken:
             self.log.info("purchasing data from the datapool")
 
-            self.log.warning(f"strategy: {strategy}")
             self.__create_envelope(
                 OceanMessage.Performative.DOWNLOAD_JOB, **{
-                    # "datatoken_address": ???,
+                    "datatoken_address": "unused",
                     "datatoken_amt": 2,
                     "max_cost_ocean": 1,
-                    # "asset_did": ???,
+                    "asset_did": "unused",
                     "pool_address": strategy.purchased_data["datapool_address"],
                 }
             )
@@ -206,10 +205,10 @@ class OceanC2DBehaviour(Behaviour):
 
             self.__create_envelope(
                 OceanMessage.Performative.DOWNLOAD_JOB, **{
-                    # "datatoken_address": ???,
+                    "datatoken_address": "unused",
                     "datatoken_amt": 2,
                     "max_cost_ocean": 1,
-                    # "asset_did": ???,
+                    "asset_did": "unused",
                     "pool_address": strategy.purchased_data["algpool_address"],
                 }
             )
