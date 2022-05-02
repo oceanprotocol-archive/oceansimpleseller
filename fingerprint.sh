2b1e6c6d60e621edd30795811f00bcaee50201d1
David Hunt-Mateo <david@oceanprotocol.com>
2022-05-02 08:51:19 -0400

    Add script for easily fingerprinting components

---
+++ fingerprint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Run this script from within a pipenv shell.
+aea fingerprint by-path src/packages/eightballer/connections/ocean/
+aea fingerprint by-path src/packages/eightballer/connections/storj_file_transfer/
+aea fingerprint by-path src/packages/eightballer/protocols/file_storage/
+aea fingerprint by-path src/packages/eightballer/protocols/ocean/
+aea fingerprint by-path src/packages/eightballer/skills/ocean_seller/
+aea fingerprint by-path src/packages/eightballer/skills/ocean_buyer/
