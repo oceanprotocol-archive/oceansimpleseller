74d064f1a662f6e469d372874eb7f6cda13f2f6c
Maria Carmina <mariacarmina.cretu@gmail.com>
2022-12-15 15:29:50 +0200

    Fix import of web3.

--- src/distribute_ocean_tokens.py
+++ src/distribute_ocean_tokens.py
@@ -3,6 +3,7 @@
 import os
 from typing import List
 from brownie.network import accounts
+from web3.main import Web3
 from ocean_lib.example_config import get_config_dict
 from ocean_lib.models.datatoken import Datatoken
 from ocean_lib.ocean.ocean import Ocean
@@ -33,7 +34,7 @@ if __name__ == "__main__":
 
     config = get_config_dict("development")
     ocean = Ocean(config)
-    amount = ocean.web3.toWei(10000, "ether")
+    amount = Web3.toWei(10000, "ether")
     ocean_deployer_wallet = accounts.add(os.getenv("FACTORY_DEPLOYER_PRIVATE_KEY"))
 
     recipients = []
