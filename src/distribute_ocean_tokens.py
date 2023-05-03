8cf7257bdab8faaac640751cb61a09d84d3c2f1b
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-05-03 20:24:50 +0300

    Fix distributing fake OCEANs function.

--- src/distribute_ocean_tokens.py
+++ src/distribute_ocean_tokens.py
@@ -5,7 +5,7 @@ from typing import List
 from brownie.network import accounts
 from web3.main import Web3
 from ocean_lib.example_config import get_config_dict
-from ocean_lib.models.datatoken import Datatoken
+from ocean_lib.models.datatoken_base import DatatokenBase
 from ocean_lib.ocean.ocean import Ocean
 from ocean_lib.ocean.util import get_ocean_token_address
 from ocean_lib.web3_internal.utils import connect_to_network
@@ -20,7 +20,7 @@ def distribute_ocean_tokens(
     """
     Mint OCEAN tokens to seller and buyer
     """
-    OCEAN_token = Datatoken(
+    OCEAN_token = DatatokenBase.get_typed(
         ocean.config_dict, address=get_ocean_token_address(ocean.config_dict)
     )
 
